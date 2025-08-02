8bb77df6411191a7bb6e31b40fb0c0bd10d8f63b
rdalx42 <hopartean.radu@spectrum.ro>
2025-08-02 19:30:22 +0300

    removed a couple of tiles due to new sprite_manager module

--- SurvivalGame/data/tiles.lua
+++ SurvivalGame/data/tiles.lua
@@ -1,32 +1,20 @@
 
 local tiles = {
-    [-1] = { name = "darkwater", image = nil, color = {0.05, 0.07, 0.2} },   
-    [0] = { name = "water", image = nil, color = {0.1, 0.3, 0.5} },          
-    [1] = { name = "grass", image = nil, color = {0.3, 0.5, 0.2} },         
+    [-1] = { name = "darkwater", image = nil, color = {0.05, 0.07, 0.2} },    
+    [0] = { name = "water", image = nil, color = {0.1, 0.3, 0.5} },           
+    [1] = { name = "grass", image = nil, color = {0.3, 0.5, 0.2} },          
     [2] = { name = "mountain", image = nil, color = {0.5, 0.48, 0.44} },     
-    [3] = { name = "darkgrass", image = nil, color = {0.2, 0.3, 0.1} },      
-    [4] = { name = "sand", image = nil, color = {0.76, 0.7, 0.55} },         
-    [5] = { name = "rock", image = nil, color = {0.4, 0.4, 0.38} },         
-    [6] = { name = "stone", image = nil, color = {0.65, 0.64, 0.6} },         
+    [3] = { name = "darkgrass", image = nil, color = {0.2, 0.3, 0.1} },       
+    [4] = { name = "sand", image = nil, color = {0.76, 0.7, 0.55} },          
+    [5] = { name = "rock", image = nil, color = {0.4, 0.4, 0.38} },           
+    [6] = { name = "stone", image = nil, color = {0.65, 0.64, 0.6} },       
     [7] = { name = "darkstone", image = nil, color = {0.25, 0.25, 0.25} }, 
     [8] = { name = "dirt", image = nil, color = {0.35, 0.25, 0.1} },         
-    [9] = { name = "snow", image = nil, color = {0.9, 0.9, 0.9} },           
-    [10] = { name = "smallrock", image = nil, returntype = 6, color = {0.55, 0.55, 0.5}, hp = 10} ,
-    [11] = {name = "treebottom", image = nil , returntype = 1, color = {0.55,0.55,0.5},hp=10},
-    [12] = {name = "treetop", image = nil , color = {0.55,0.55,0.5},returntype = 1},
+    [9] = { name = "snow", image = nil, color = {0.9, 0.9, 0.9} },            
+    --[10] = { name = "smallrock", image = nil, returntype = 6, color = {0.55, 0.55, 0.5}, hp = 10} ,
+   -- [11] = {name = "treebottom", image = nil , returntype = 1, color = {0.55,0.55,0.5},hp=10},
+   -- [12] = {name = "treetop", image = nil , color = {0.55,0.55,0.5},returntype = 1},
 }
-
-
-local tiles_with_images = {10,11,12} 
-
-function tiles.loadImages()
-    for i, id in ipairs(tiles_with_images) do
-        local tile = tiles[id]
-        if tile then
-            tile.image = love.graphics.newImage("assets/tiles/" .. tile.name .. ".png")
-        end
-    end
-end
 
 function tiles.selection(n)
     if n < 0.15 then return -1
@@ -42,4 +30,3 @@ function tiles.selection(n)
 end
 
 return tiles
-
