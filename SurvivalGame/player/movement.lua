3b32782ec21da2653366fdd89ab8cd7c19198ba7
rdalx42 <hopartean.radu@spectrum.ro>
2025-08-07 18:02:19 +0300

    added a holding item which will rotate 360 degrees around cam

--- SurvivalGame/player/movement.lua
+++ SurvivalGame/player/movement.lua
@@ -4,6 +4,8 @@ local movement = {
     movement_keys = {"w","a","s","d","up","down","left","right"},
 
     player_object = {x = 0, y = 0, sprite = nil, speed = 400, width=16, height=16},
+    tool_object = {offset_x = 0,offset_y = 0,x = 0,y = 0,sprite = nil},
+    tobj_sprite = love.graphics.newImage("assets/sprites/leaf.png")
 }
 
 local moveUp = function(self, dt)
@@ -40,16 +42,45 @@ movement.key_bindings = {
     right = moveRight,
 }
 
+function movement.get_orientation()
+    
+    local mx, my = love.mouse.getPosition()
+    local wx, wy = cam:worldCoords(mx,my)
+    local dx = wx-movement.player_object.x 
+    local dy = wy-movement.player_object.y 
+    local angle = math.atan2(dy,dx)
+    local r = 30
+
+    movement.tool_object.x = movement.player_object.x+math.cos(angle)*r 
+    movement.tool_object.y=movement.player_object.y+math.sin(angle)*r 
+    movement.tool_object.angle=angle
+end
+
+function movement.set_tool(img)
+
+    if not img then 
+        movement.tobj_sprite=love.graphics.newImage("assets/sprites/TRANSPARENT.png") -- placeholder for images which dont exists
+    else
+        movement.tobj_sprite=img 
+    end
+    movement.tool_object.sprite = movement.tobj_sprite
+    movement.tool_object.offset_x = 0
+    movement.tool_object.offset_y = 0
+end
+
 function movement.set_player(object, speed)
     movement.player_object.speed = speed or 200
     movement.player_object.sprite = object.sprite
     movement.player_object.x = object.x
     movement.player_object.y = object.y
+    movement.tool_object.sprite = movement.tobj_sprite
+    movement.tool_object.x=movement.player_object.x 
+    movement.tool_object.y=movement.player_object.y
 end
 
 function movement.get(dt)
     local player = movement.player_object
-
+    
     for _, key in ipairs(movement.movement_keys) do
         if love.keyboard.isDown(key) and movement.key_bindings[key] then
             local newX, newY = movement.key_bindings[key](movement, dt)
